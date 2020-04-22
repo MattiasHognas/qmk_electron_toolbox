5d81fbe9d36621c5148802afbc2082f9c4bdb61a
Mattias Högnäs <mattiaslab@outlook.com>
2020-04-22 19:02:17 +0200

    Fixed unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Bridge } from './types/Bridge';
+
+const mockBridge: Bridge = {
+    sendTestRequest(): void {
+        console.log('NO BRIDGE: sendTestRequest called');
+    },
+    attachTestResponseListener(): void {
+        console.log('NO BRIDGE: attachTestResponseListener called');
+    },
+    removeTestResponseListener(): void {
+        console.log('NO BRIDGE: removeTestResponseListener called');
+    },
+};
+window.api = mockBridge;
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
